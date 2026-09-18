a30c2829cd4b8f2e7817552095f59bb3c28dd60a
axm6050 <labuser@CSE137915.localdomain>
2026-09-18 14:33:51 -0500

    Code1.c

--- Code1.c
+++ Code1.c
@@ -3,12 +3,15 @@
 int main()
 {
     char Name[50] = {};
-
+    int num;
 
     printf("This code will generate a personalized welcome.\nPlease enter your name: ");
     scanf(" %s", Name);
 
-    printf("Hello %s!! This is the cse1106 lab. Welcome!\n", Name);
+    printf("Hello %s!! This is the cse1106 lab. Welcome!\nWhen you are ready to exit, type 1 or 0 and hit enter...", Name);
+    scanf("%d", &num);
+
+    printf("Wow. Cya! K Bye!!!!\n");
 
     return 0;
 }
